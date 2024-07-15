3d3cdda4b5314e484b8797d297c143bf66a6088f
Jared Tichacek <jared.tichacek@gmail.com>
2024-07-15 10:05:33 -0500

    added astro island greeting in preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://thought-transcriber.netlify.app/"
+  site: "https://thought-transcriber.netlify.app/",
+  integrations: [preact()]
 });
